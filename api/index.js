6c6602ec6f3680faaa3955614b3526c124903e79
Mafath <ahamedmafath550@gmail.com>
2024-12-30 18:53:46 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 dotenv.config();
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 mongoose.connect(process.env.MONGO)
 .then(() => {
@@ -23,3 +24,4 @@ app.listen(3000, () => {
 
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
